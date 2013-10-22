191c2a5d6661fe3a23791c85ef35858a89f33177
Sumit Agrawal <sumit4iit@gmail.com>
2013-10-22 23:54:49 +0530

    testing this

--- jquery-firefly-0.2.js
+++ jquery-firefly-0.2.js
@@ -24,60 +24,42 @@
 	 * Plugin defaults 
 	 */
 	var defaults = {
-			images : [
-					'spark.png', 
-					'spark2.png',
-					'spark3.png', 
-					'spark4.png'],
-
-			total : 40,
+			images : ['http://dharmmotyar.googlecode.com/svn/trunk/images/spark.png', 'http://dharmmotyar.googlecode.com/svn/trunk/images//spark2.png',
+			'http://dharmmotyar.googlecode.com/svn/trunk/images/spark3.png', 'http://dharmmotyar.googlecode.com/svn/trunk/images/spark4.png'],
+			total : 10,
 			ofTop: 0,
 			ofLeft: 0,
-			ofBottom: -1,
-			ofRight: -1,
+			on:'document.body'
 
-			on: document.body
 	};
 	
+
 	$.firefly = function(settings) {
 			$.firefly.settings = $.extend({}, defaults, settings);
 			$.firefly.eleHeight = $($.firefly.settings.on).height();
 			$.firefly.eleWidth = $($.firefly.settings.on).width();
-			
-			
-			$.firefly.offsetTop = $.firefly.settings.ofTop;
-			$.firefly.offsetLeft = $.firefly.settings.ofLeft;
-			
-			if($.firefly.settings.ofBottom == -1)
-			{
-				$.firefly.offsetBottom = $.firefly.settings.ofTop;
+			if($.firefly.settings.on!=='document.body'){
+				var off = $($.firefly.settings.on).offset();
+				$.firefly.offsetTop = off.top;
+				$.firefly.offsetLeft = off.left;
+				$.firefly.eleHeight = $($.firefly.settings.on).height();
+				$.firefly.eleWidth = $($.firefly.settings.on).width();
 			}
-			else
-			{
-				$.firefly.offsetBottom = $.firefly.settings.ofBottom;
-			}
-			
-			if($.firefly.settings.ofRight == -1)
-			{
-				$.firefly.offsetRight  = $.firefly.settings.ofLeft;
-			}
-			else
-			{
-				$.firefly.offsetRight = $.firefly.settings.ofRight;
-			}	
+			else{
+				$.firefly.offsetTop = 0;
+				$.firefly.offsetLeft = 0;
+				$.firefly.eleHeight = $(document.body).height();
+				$.firefly.eleWidth = $(document.body).width();
 
-		
-			$.firefly.eleHeight = $($.firefly.settings.on).height();
-			$.firefly.eleWidth = $($.firefly.settings.on).width();
-			
-			if($.firefly.preloadImages())
-			{
-				for (i = 0; i < $.firefly.settings.total; i++)
-				{
- 					 $.firefly.fly($.firefly.create($.firefly.settings.images[$.firefly.random(($.firefly.settings.images).length)]));
-				}
 			}
+
 			
+		
+			if($.firefly.preloadImages()){
+			for (i = 0; i < $.firefly.settings.total; i++){
+				 $.firefly.fly($.firefly.create($.firefly.settings.images[$.firefly.random(($.firefly.settings.images).length)]));
+			}
+			}
 			return;
 	};
 	
@@ -85,55 +67,45 @@
 	 * Public Functions
 	 */
 
-	 $.firefly.create = function(img)
-	 {
+	 $.firefly.create = function(img){
 					spark = $('<img>').attr({'src' : img}).hide();
-					$(document.body).append(spark);
-
+					
+					 $($.firefly.settings.on).append(spark);
 							return spark.css({
-								            'position':'absolute',												
+								            'position':'absolute',
+												
 										    'z-index': -1*$.firefly.random(20), //under all the stuff
-											top: $.firefly.offsetTop + $.firefly.random($.firefly.eleHeight-$.firefly.offsetBottom),	//offsets
-											left:  $.firefly.offsetLeft + $.firefly.random($.firefly.eleWidth-$.firefly.offsetRight)   //offsets
-											}).show();
+											top: $.firefly.offsetTop + $.firefly.random(($.firefly.eleHeight-50)),	//offsets
+											left:  $.firefly.offsetLeft + $.firefly.random(($.firefly.eleWidth-50)) //offsets
+											}).show();		
 	 }
+    
 
-$.firefly.fly = function(sp)
-{
+
+$.firefly.fly = function(sp) {
+	
   $(sp).animate({
-	top: $.firefly.offsetTop + $.firefly.random($.firefly.eleHeight-$.firefly.offsetBottom),	//offsets
-	left:  $.firefly.offsetLeft + $.firefly.random($.firefly.eleWidth-$.firefly.offsetRight),   //offsets
-	opacity: $.firefly.opacity()
-  }, (($.firefly.random(10) + 5) * 2000),function(){   $.firefly.fly(sp)   } );
+	  top: $.firefly.offsetTop + $.firefly.random(($.firefly.eleHeight-50)),	//offsets
+	  left: $.firefly.offsetLeft + $.firefly.random(($.firefly.eleWidth-50))
+  }, (($.firefly.random(10) + 5) * 2000),function(){  $.firefly.fly(sp) } );
 };
 
-$.firefly.stop = function(sp)
-{
-	$(sp).stop();
+$.firefly.stop = function(sp) {
+  $(sp).stop();
 };
 
-$.firefly.preloadImages = function()
-{
+
+$.firefly.preloadImages = function() {
 	var preloads = new Object();
-	for (i = 0; i < ($.firefly.settings.images).length; i++)
-	{
-			preloads[i] = new Image();
-			preloads[i].src =  $.firefly.settings.images[i];
-	}
+	for (i = 0; i < ($.firefly.settings.images).length; i++){  
+			preloads[i] = new Image(); preloads[i].src =  $.firefly.settings.images[i];
+        }
 	return true;
 }
 
 $.firefly.random = function(max) {
 	return Math.ceil(Math.random() * max) - 1;
 }
-
-$.firefly.opacity = function()
-{
-	op= Math.random();
-	if(op < .2)
-		return 0;
-	else
-		return 1;
-}
-	
+		
 })(jQuery);
+
--- jquery.firefly.js
+++ jquery.firefly.js
@@ -1,80 +1,62 @@
-	/*
-	* jQuery Firefly v0.1
-	* https://github.com/motyar/firefly
-	
-	* Licensed under the MIT license.
-	* Copyright 2011 Dharmveer Motyar
-	* http://motyar.blogspot.com
-	*/
-	(function($) {
-	
-		// sparks images
-		/* Plugin defaults */
-		var defaults = {
-			images : [
-				'spark.png',
-				'spark2.png',
-				'spark3.png',
-				'spark4.png'],
-			total : 40
-		};
-		
-		$.firefly = function(settings) {
-			$.firefly.settings = $.extend({}, defaults, settings);
-				if($.firefly.preloadImages()){
-					for (i = 0; i < $.firefly.settings.total; i++){
-						$.firefly.fly($.firefly.create($.firefly.settings.images[$.firefly.random(($.firefly.settings.images).length)]));
-					}
-				}
-			return;
-		};
-		
-		
-		/* Public Functions */
-		$.firefly.create = function(img){
-			spark = $('<img>').attr({'src' : img}).hide();
-			$(document.body).append(spark);
-				return spark.css({
-					'position':'absolute',
-					'z-index': $.firefly.random(20),
-					top: $.firefly.random(($(window).height()- 150 )),	//offsets
-					left: $.firefly.random(($(window).width()- 150 ))	//offsets
-					}).show();
-	}
-	
-	$.firefly.fly = function(sp) {
-		$(sp).animate({
-			top: $.firefly.random(($(window).height()-150 )),	//offsets
-			left: $.firefly.random(($(window).width()- 150 )),
-			opacity: $.firefly.opacity() 		
-			},
-		
-		(($.firefly.random(10) + 5) * 1100 ),
-			
-		function(){ $.firefly.fly(sp) } );
+/*
+* jQuery Firefly v0.1
+* https://github.com/motyar/firefly
+
+* Licensed under the MIT license.
+* Copyright 2011 Dharmveer Motyar
+* http://motyar.blogspot.com
+*/
+(function($) {
+
+	/* Plugin defaults */
+	var defaults = {
+		images : [
+			'http://dharmmotyar.googlecode.com/svn/trunk/images/spark.png',
+			'http://dharmmotyar.googlecode.com/svn/trunk/images//spark2.png',
+			'http://dharmmotyar.googlecode.com/svn/trunk/images/spark3.png',
+			'http://dharmmotyar.googlecode.com/svn/trunk/images/spark4.png'],
+		total : 40
 	};
 	
-	$.firefly.preloadImages = function() {
-		var preloads = new Object();
-		for (i = 0; i < ($.firefly.settings.images).length; i++){
-				preloads[i] = new Image(); preloads[i].src = $.firefly.settings.images[i];
+	$.firefly = function(settings) {
+		$.firefly.settings = $.extend({}, defaults, settings);
+			if($.firefly.preloadImages()){
+				for (i = 0; i < $.firefly.settings.total; i++){
+					$.firefly.fly($.firefly.create($.firefly.settings.images[$.firefly.random(($.firefly.settings.images).length)]));
+				}
 			}
-		return true;
-	}
+		return;
+	};
 	
-	$.firefly.random = function(max) {
-		return Math.ceil(Math.random() * max) - 1;
-	}
-	
-	$.firefly.opacity = function()
-	{
-		op = Math.random();
-		if(op < .2)
-			return 0;
-		else
-			return 1;
-	}
+	/* Public Functions */
+	$.firefly.create = function(img){
+		spark = $('<img>').attr({'src' : img}).hide();
+		$(document.body).append(spark);
+			return spark.css({
+				'position':'absolute',
+				'z-index': $.firefly.random(20),
+				top: $.firefly.random(($(window).height()-150)),	//offsets
+				left: $.firefly.random(($(window).width()-150))		//offsets
+				}).show();
+}
 
-	// let values of opacity range from 0 to val and random.
-	})(jQuery);
-	
+$.firefly.fly = function(sp) {
+	$(sp).animate({
+		top: $.firefly.random(($(window).height()-150)),	//offsets
+		left: $.firefly.random(($(window).width()-150))
+	}, (($.firefly.random(10) + 5) * 1100),function(){ $.firefly.fly(sp) } );
+};
+
+$.firefly.preloadImages = function() {
+	var preloads = new Object();
+	for (i = 0; i < ($.firefly.settings.images).length; i++){
+			preloads[i] = new Image(); preloads[i].src = $.firefly.settings.images[i];
+		}
+	return true;
+}
+
+$.firefly.random = function(max) {
+	return Math.ceil(Math.random() * max) - 1;
+}
+
+})(jQuery);
